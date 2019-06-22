b66e3dd6c5436e6b1d5bf0e2c3ad2c2f7075f199
xryuseix <ryusei143.shootingstar@gmail.com>
2019-06-22 16:01:24 +0900

    shuffle修正

--- cubing_main.cpp
+++ cubing_main.cpp
@@ -1,19 +1,20 @@
 #include <vector>
 
-using CubeType = std::vector<std::vector<char>>;
-
 struct Cube {
-    CubeType cubing;
+    std::vector<std::vector<char>> cubing;
     Cube () {
       // write here
     }
   
-    void encryupt() {
-        cubing = hash(cubing);
-        cubing = sequential(cubing);
-        cubing = randnum(cubing);
-        cubing = cubing_en(cubing, key);
-        cubing = shuffle(cubing);
+    void shuffle() {
+        for(int i = cubing.size() - 1; i > 0; i--) {
+            int j = rand() % i;
+            for(int k = 0; k < 54; k++) {
+                char c = cubing[i][k];
+                cubing[i][k] = cubing[j][k];
+                cubing[j][k] = c;
+            }    
+        }
     }
   
     void print() {
@@ -28,7 +29,7 @@ struct Cube {
 
 int main(int ac, char **av) {
     Cube cube;
-    cube.encryupt();
+    cube.shuffle();
     cube.print();
     return 0;
 }
