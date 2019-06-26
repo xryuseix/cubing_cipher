d80cdecbddc0078431d3088aa97934eabb46bce9
xryuseix <ryusei143.shootingstar@gmail.com>
2019-06-26 17:41:31 +0900

    rotate実装

--- cubing_main.cpp
+++ cubing_main.cpp
@@ -21,49 +21,49 @@ struct Cube {
   
     void rotate(CubeOP* key, int klength) {
         for(int i = 0; i < klength; i = i + 3) { // 転置
-            int num = 2 + key[i + 2] - '3'; // 型変換('1'->0,'2'->1)
             char letter;
-            
-            if(key[i] == '1') { // 方向 // 縦方向 
-                for(int k = 0; k < 3 - ('3' - key[i + 2]); k++) { // 回数 
-                    letter=cubing[0 + num]; // numは転値対象列(0~2)
-                    
-                    cubing[0 + num] = cubing[3 + num];
-                    cubing[3 + num] = cubing[6 + num];
-                    cubing[6 + num] = cubing[12 + num];
-                    cubing[12 + num] = cubing[24 + num];
-                    cubing[24 + num] = cubing[36 + num];
-                    cubing[36 + num] = cubing[45 + num];
-                    cubing[45 + num] = cubing[48 + num];
-                    cubing[48 + num] = cubing[51 + num];
-                    cubing[51 + num] = cubing[44 - num];
-                    cubing[44 - num] = cubing[32 - num];
-                    cubing[32 - num] = cubing[20 - num];
-                    cubing[20 - num] = letter;
+            int col = key[i].column - 1;
+            if(key[i].direction == 1) { // 方向 // 縦方向 
+                for(int k = 0; k < key[i].times; k++) { // 回数 
+
+                    letter=cubing[0 + col]; // colは転値対象列(0~2)
+                    cubing[0 + col] = cubing[3 + col];
+                    cubing[3 + col] = cubing[6 + col];
+                    cubing[6 + col] = cubing[12 + col];
+                    cubing[12 + col] = cubing[24 + col];
+                    cubing[24 + col] = cubing[36 + col];
+                    cubing[36 + col] = cubing[45 + col];
+                    cubing[45 + col] = cubing[48 + col];
+                    cubing[48 + col] = cubing[51 + col];
+                    cubing[51 + col] = cubing[44 - col];
+                    cubing[44 - col] = cubing[32 - col];
+                    cubing[32 - col] = cubing[20 - col];
+                    cubing[20 - col] = letter;
+
                 }
-            } else if(key[i] == '2'){ // 横方向
-                for(int k = 0; k < 3 - ('3' - key[i + 2]); k++) {
-                    letter = cubing[9 + 12*num];
+            } else if(key[i].direction == 2){ // 横方向
+                for(int k = 0; k < key[i].times; k++) { // 回数 
+                    letter = cubing[9 + 12*col];
                     for(int m = 0; m < 11; m++) {
-                        cubing[9 + 12*num + m] = cubing[10 + 12*num + m];
+                        cubing[9 + 12*col + m] = cubing[10 + 12*col + m];
                     }
-                    cubing[20 + 12*num] = letter;
+                    cubing[20 + 12*col] = letter;
                 }
-            } else if(key[i] == '3') { // 回転方向
-                for(int k = 0; k < 3 - ('3' - key[i + 2]); k++) {
-                    letter = cubing[6 - 3*num];
-                    cubing[6 - 3*num] = cubing[7 - 3*num];
-                    cubing[7 - 3*num] = cubing[8 - 3*num];
-                    cubing[8 - 3*num] = cubing[15 + num];
-                    cubing[15 + num] = cubing[27 + num];
-                    cubing[27 + num] = cubing[39 + num];
-                    cubing[39 + num] = cubing[47 + 3*num];
-                    cubing[47 + 3*num] = cubing[46 + 3*num];
-                    cubing[46 + 3*num] = cubing[45 + 3*num];
-                    cubing[45 + 3*num] = cubing[35 - num];
-                    cubing[35 - num] = cubing[23 - num];
-                    cubing[23 - num] = cubing[11 - num];
-                    cubing[11 - num] = letter;
+            } else if(key[i].direction == 3) { // 回転方向
+                for(int k = 0; k < key[i].times; k++) { // 回数 
+                    letter = cubing[6 - 3*col];
+                    cubing[6 - 3*col] = cubing[7 - 3*col];
+                    cubing[7 - 3*col] = cubing[8 - 3*col];
+                    cubing[8 - 3*col] = cubing[15 + col];
+                    cubing[15 + col] = cubing[27 + col];
+                    cubing[27 + col] = cubing[39 + col];
+                    cubing[39 + col] = cubing[47 + 3*col];
+                    cubing[47 + 3*col] = cubing[46 + 3*col];
+                    cubing[46 + 3*col] = cubing[45 + 3*col];
+                    cubing[45 + 3*col] = cubing[35 - col];
+                    cubing[35 - col] = cubing[23 - col];
+                    cubing[23 - col] = cubing[11 - col];
+                    cubing[11 - col] = letter;
                 }
             }
         }
