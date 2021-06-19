15ea08f8ddea20c7f158bbf42ae165fd1b2e33bf
xryuseix <ryusei143.shootingstar@gmail.com>
2021-06-19 16:51:01 +0900

    fix: 鍵生成アルゴリズムの型を修正

--- src/cubing/src/key.rs
+++ src/cubing/src/key.rs
@@ -1,26 +1,32 @@
-// pub use crate::cube;
-// use rand::Rng;
+pub use crate::cube;
+use rand::Rng;
 
-// pub fn generate(k: usize) -> Vec<cube::CubeOP> {
-//     let mut rng = rand::thread_rng();
-//     let key_vec: Vec<u8> = (0..k*3).map(|_| rng.gen_range(0..3)).collect();
-//     let mut key = Vec::new();
-//     for k in key_vec {
+pub fn generate(k: usize) -> Vec<cube::CubeOP> {
+    let mut rng = rand::thread_rng();
+    let mut key = Vec::new();
+    for _ in 0..k {
+        key.push(cube::CubeOP {
+            direction: rng.gen_range(0..3),
+            column: rng.gen_range(0..3),
+            times: rng.gen_range(0..3),
+        })
+    }
+    key
+}
 
-//     }
-// }
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-
-//     #[test]
-//     fn generate_test() {
-//         let key_len = 90;
-//         let key = generate(key_len);
-//         assert_eq!(key.len(), key_len);
-//         for i in 0..key_len {
-//             assert!(key[i] <= 2);
-//         }
-//     }
-// }
+    #[test]
+    fn generate_test() {
+        let key_len = 90;
+        let key = generate(key_len);
+        assert_eq!(key.len(), key_len);
+        for k in key {
+            assert!(k.direction <= 2);
+            assert!(k.column <= 2);
+            assert!(k.times <= 2);
+        }
+    }
+}
